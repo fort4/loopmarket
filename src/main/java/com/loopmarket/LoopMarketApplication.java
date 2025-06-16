ed85b1d0b137d3b109ea677f2c771474f2103223
karaka18 <hhsky7168@gmail.com>
2025-06-16 12:05:20 +0900

    war 배포버전

--- src/main/java/com/loopmarket/LoopMarketApplication.java
+++ src/main/java/com/loopmarket/LoopMarketApplication.java
@@ -2,20 +2,21 @@ package com.loopmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder; // 추가
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; // 추가
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-/**
- * Spring Boot에서 애플리케이션을 실행하는 시작점이에요. 
- * (C언어나 Java의 main() 함수같은 기능)
- * Configuratio, EnableAutoConfiguration, ComponentScan 3가지 기능을 포함합니다.
- * 구매 10일 후 자동 구매 확정 처리를 위해 @EnableScheduling 추가하였습니다.
- */
 @EnableScheduling
 @SpringBootApplication
-public class LoopMarketApplication {
+public class LoopMarketApplication extends SpringBootServletInitializer { // 상속 추가
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoopMarketApplication.class, args);
-	}
+    // WAR 배포 시 서블릿 컨테이너가 이 메서드로 초기화함
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LoopMarketApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(LoopMarketApplication.class, args);
+    }
 }
